8bbe93e21a139112406bcfa699839f460625c98c
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2024-11-28 15:02:23 +0530

    change in gitignore file

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VqzwyVfL2SCJhb0JM3MQSPr8+ftg+CEAwBXtlu2xap8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
